0a65de7f804ea66a167e45eebafc58ce50fe05ec
Michael McGranaghan <M.McGranaghan@opustech.co.uk>
2022-11-07 08:26:39 +0000

    add chatcharts and chatter classes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { MetricModule, FirstEncounter } from "./lib/metricmodules.js";
+import { ChatChart } from "./lib/chatchart.js";
+import { Chatter } from "./lib/chatter.js";
 
 export {
+    ChatChart,
+    Chatter,
     MetricModule,
     FirstEncounter
 }
