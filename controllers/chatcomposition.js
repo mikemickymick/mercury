227f1bd3f451998760cced7eb251724e28af34aa
Michael McGranaghan <M.McGranaghan@opustech.co.uk>
2022-11-14 17:17:49 +0000

    add chatcomposition.js

---
+++ controllers/chatcomposition.js
@@ -0,0 +1,32 @@
+import { Chatter } from "./models/chatter.js";
+
+/**Generates the Chat composition from an array of Message objects */
+function GenerateChatComposition(messageObjectArray){
+    let chatters = new Array();
+    messageObjectArray.forEach(element => {
+        let chatterInArray = false;
+        chatters.forEach(x => {
+            if(x.Name == element.Author){
+                chatterInArray = true;
+                x.MessageCount += 1;
+            }
+        });
+        if(!chatterInArray){
+            let chatter = new Chatter(messageObjectArray.indexOf(element), element.Author, 1, 0);
+            chatters.push(chatter);
+        }
+    });            
+
+    //Add percentages
+    let totalMessages = 0;
+    chatters.forEach(m => {
+        totalMessages += m.MessageCount;
+    });
+    chatters.forEach(y => {
+        y.MessagePercent = Math.round((y.MessageCount / totalMessages) * 100);
+    });
+
+    return chatters;
+}
+
+export {GenerateChatComposition};
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { ChatComposition, FirstEncounter, InstanceCounter, MessageDays, MessageTimes, MetricModule, TopEmojis, TopWords  } from "./models/metricmodules.js";
 import { ChatChart } from "./models/chatchart.js";
-import { Chatter } from "./models/chatter.js";
 import { FormatFile } from "./controllers/dataprepper.js";
+import { GenerateChatComposition } from "./controllers/chatcomposition.js";
 
 export {
     ChatChart,
     ChatComposition,
-    Chatter,
     FirstEncounter,
     FormatFile,
+    GenerateChatComposition,
     InstanceCounter,
     MessageDays,
     MessageTimes,
