bc2012795c53117055b59037ee3e9c51efb90a61
Michael McGranaghan <M.McGranaghan@opustech.co.uk>
2022-11-14 07:29:21 +0000

    add export to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ChatComposition, FirstEncounter, InstanceCounter, MessageDays, MessageTimes, MetricModule, TopEmojis, TopWords  } from "./models/metricmodules.js";
 import { ChatChart } from "./models/chatchart.js";
 import { Chatter } from "./models/chatter.js";
-import { FormatFile } from "./controllers/dataprepper.js";
+import { FormatFile, FormatIOSChats } from "./controllers/dataprepper.js";
 
 export {
     ChatChart,
@@ -9,6 +9,7 @@ export {
     Chatter,
     FirstEncounter,
     FormatFile,
+    FormatIOSChats,
     InstanceCounter,
     MessageDays,
     MessageTimes,
