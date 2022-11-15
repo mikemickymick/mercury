4b59ac11044f808d7b797e4d0f5f9652a8de8966
Michael McGranaghan <M.McGranaghan@opustech.co.uk>
2022-11-15 06:52:22 +0000

    try to fix chatter

--- models/chatter.js
+++ models/chatter.js
@@ -1,8 +1,8 @@
 /**Represents a person within a chat*/
 class Chatter {
-    constructor(index, author, messageCount, messagePercent){
-        this.Index = index;
-        this.Name = author;
+    constructor(authorIndex, author, messageCount, messagePercent){
+        this.AuthorIndex = authorIndex;
+        this.Author = author;
         this.MessageCount = messageCount;
         this.MessagePercent = messagePercent;
     };
