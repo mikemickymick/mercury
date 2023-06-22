3b0db978fd64ad8c18be5e811beed6aca552dcda
Michael McGranaghan <michaelmcgranaghan@formuspro.com>
2023-06-22 18:07:10 +0100

    metricmodulecontroller refactor 2

--- controllers/metricmodulecontroller.js
+++ controllers/metricmodulecontroller.js
@@ -4,65 +4,56 @@ import { ChatComposition, FirstEncounter, MessageDays, MessageTimes, SearchRecor
 import { AudioArray, EmojiArray, ImageArray, LateNightArray, LaughArray, LoveArray, MorningArray, NightArray, NumberRegEx, PunctuationRegEx, SkipWords, SwearArray } from '../helpers/searchhelper.js';
 
 /**Generates the Chat composition from an array of Message objects */
-function GenerateChatComposition(messageObjectArray){
-    let chatters = new Array();
+function GenerateChatComposition(messageObjectArray) {
+    const chatters = new Map();
     let authorIndex = 0;
-    messageObjectArray.forEach(element => {
-        let chatterInArray = false;
-        chatters.forEach(x => {
-            if(x.Name == element.Author){
-                chatterInArray = true;
-                x.MessageCount += 1;
-            }
-        });
-        if(!chatterInArray){
-            authorIndex ++;
-            let chatter = new Chatter(authorIndex, element.Author, 1, 0);
-            chatters.push(chatter);
+    
+    for (const element of messageObjectArray) {
+        const author = element.Author;
+        if (chatters.has(author)) {
+            const chatter = chatters.get(author);
+            chatter.MessageCount += 1;
+        } else {
+            authorIndex++;
+            const chatter = new Chatter(authorIndex, author, 1, 0);
+            chatters.set(author, chatter);
         }
-    });            
-
-    //Add percentages
-    let totalMessages = 0;
-    chatters.forEach(m => {
-        totalMessages += m.MessageCount;
-    });
-    chatters.forEach(y => {
-        y.MessagePercent = Math.round((y.MessageCount / totalMessages) * 100);
-    });
-
-    return new ChatComposition(chatters);
+    }
+    
+    const totalMessages = messageObjectArray.length;
+    for (const chatter of chatters.values()) {
+        chatter.MessagePercent = Math.round((chatter.MessageCount / totalMessages) * 100);
+    }
+    
+    return new ChatComposition([...chatters.values()]);
 }
 
 /**Generates a First Encounter module */
-function GenerateFirstEncounter(chatObjArr){
-    let firstMessage = chatObjArr[0];
-    let firstMessageDate = firstMessage["Date"];
-    let firstMessageTime = firstMessage["Time"];
-    let firstMessageAuthor = firstMessage["Author"];
-    let firstMessageBody = firstMessage["MessageBody"];
-
-    let replierIndex = chatObjArr.indexOf(chatObjArr.find(x => x.Author != firstMessageAuthor));
-    let replyMessage = chatObjArr[replierIndex];
-    let replyDate = replyMessage["Date"];
-    let replyTime = replyMessage["Time"];
-    let replyAuthor = replyMessage["Author"];
-    let replyMessageBody = replyMessage["MessageBody"];
-
-    let arrFromSecondAuth = chatObjArr.slice(replierIndex,50);
-    let thirdAuthorIndex = arrFromSecondAuth.indexOf(arrFromSecondAuth.find(x => x.Author != replyAuthor));
-
+function GenerateFirstEncounter(chatObjArr) {
+    const firstMessage = chatObjArr[0];
+    const firstMessageDate = firstMessage.Date;
+    const firstMessageTime = firstMessage.Time;
+    const firstMessageAuthor = firstMessage.Author;
+    let firstMessageBody = firstMessage.MessageBody;
+    
+    const replierIndex = chatObjArr.findIndex(x => x.Author !== firstMessageAuthor);
+    const replyMessage = chatObjArr[replierIndex];
+    const replyDate = replyMessage.Date;
+    const replyTime = replyMessage.Time;
+    const replyAuthor = replyMessage.Author;
+    let replyMessageBody = replyMessage.MessageBody;
+    
     firstMessageBody = GetMessageComposite(chatObjArr, replierIndex, firstMessageBody);
-    replyMessageBody = GetMessageComposite(arrFromSecondAuth, thirdAuthorIndex, replyMessageBody);
-
+    replyMessageBody = GetMessageComposite(chatObjArr.slice(replierIndex, 50), chatObjArr.findIndex(x => x.Author !== replyAuthor), replyMessageBody);
+    
     return new FirstEncounter(firstMessageDate, firstMessageTime, firstMessageAuthor, firstMessageBody, replyDate, replyTime, replyAuthor, replyMessageBody);
 }
 
 /**Generates a Search Record */
-function GenerateSearchRecord(chatObjArr, searchRecordName, required, width, height, searchTermArr){
-    let searchLogs = new Array();
-
-    switch(searchRecordName){
+function GenerateSearchRecord(chatObjArr, searchRecordName, required, width, height, searchTermArr) {
+    let searchLogs = [];
+    
+    switch (searchRecordName) {
         case "laugh":
             searchTermArr = LaughArray;
             break;
@@ -91,175 +82,153 @@ function GenerateSearchRecord(chatObjArr, searchRecordName, required, width, hei
             searchTermArr = EmojiArray;
             break;
         default:
-            //Do nothing
+            // Do nothing
     }
-
-    if (searchRecordName == "emoji"){
-        let unicodeStrings = new Array();
-        searchTermArr.forEach(x => {
-            let instanceRegEx = new RegExp(x.toLowerCase(),"g");
-            let unicodeString = x.codePointAt(0).toString(16);
-            let counter = chatObjArr.filter(x => x.MessageBody.match(instanceRegEx)).length;
-
-            if (!unicodeStrings.find(v => v.uniStr === unicodeString)){
-                unicodeStrings.push({
-                    emoji: x,
-                    uniStr: unicodeString,
-                    emojiCount: counter
-                });
-            } else {
-                unicodeStrings.find((o, i) => {
-                    if (o.uniStr === unicodeString) {
-                        unicodeStrings[i] = { emoji: o.emoji, uniStr: o.uniStr, emojiCount: o.emojiCount + counter };
-                        return true; // stop searching
-                    }
-                });
-            }
+    
+    if (searchRecordName === "emoji") {
+        const unicodeStrings = new Map();
+        
+        for (const x of searchTermArr) {
+            const instanceRegEx = new RegExp(x.toLowerCase(), "g");
+            const unicodeString = x.codePointAt(0).toString(16);
+            const counter = chatObjArr.filter(x => x.MessageBody.match(instanceRegEx)).length;
             
-        });
-
-        unicodeStrings.forEach(j => {
-            let searchLog = new SearchLog(j.emoji, j.emojiCount);
-            searchLogs.push(searchLog);
-        })
-
-    }else{
-        searchTermArr.forEach(x => {
-            let instanceRegEx = new RegExp(x.toLowerCase(),"g");
-            let counter = chatObjArr.filter(x => x.MessageBody.match(instanceRegEx)).length;
-            let searchLog = new SearchLog(x, counter);
-            searchLogs.push(searchLog);
-        });
-    }
-
-    let counter = 0;
-    let orderedSearchLogs = new Array();
-    if(searchRecordName == "emoji"){
-        searchLogs.sort((a,b) => {
-            return b.Count - a.Count;
-        });
-        for(let i = 0; i < 15; i++){
-            orderedSearchLogs.push(searchLogs[i]);
+            if (unicodeStrings.has(unicodeString)) {
+                unicodeStrings.set(unicodeString, unicodeStrings.get(unicodeString) + counter);
+            } else {
+                unicodeStrings.set(unicodeString, counter);
+            }
+        }
+        
+        for (const [uniStr, emojiCount] of unicodeStrings.entries()) {
+            searchLogs.push(new SearchLog(EmojiArray[parseInt(uniStr, 16)], emojiCount));
         }
     } else {
-        searchLogs.forEach(x =>{
-            counter += x.Count
-        });
+        for (const x of searchTermArr) {
+            const instanceRegEx = new RegExp(x.toLowerCase(), "g");
+            const counter = chatObjArr.filter(x => x.MessageBody.match(instanceRegEx)).length;
+            searchLogs.push(new SearchLog(x, counter));
+        }
+    }
+    
+    let counter = 0;
+    let orderedSearchLogs = [];
+    if (searchRecordName === "emoji") {
+        searchLogs.sort((a, b) => b.Count - a.Count);
+        orderedSearchLogs = searchLogs.slice(0, 15);
+    } else {
+        for (const log of searchLogs) {
+            counter += log.Count;
+        }
         orderedSearchLogs = searchLogs;
     }
-
-    if(searchRecordName == "audio" || searchRecordName == "image"){
-        if(counter == 0){
+    
+    if (searchRecordName === "audio" || searchRecordName === "image") {
+        if (counter === 0) {
             return null;
-        }else{
-            return new SearchRecord(searchRecordName, required, width, height, orderedSearchLogs, counter);
         }
-    }else{
-        return new SearchRecord(searchRecordName, required, width, height, orderedSearchLogs, counter);
     }
+    
+    return new SearchRecord(searchRecordName, required, width, height, orderedSearchLogs, counter);
 }
 
 /**Generates a Message Days metric module */
-function GenerateMessageDays(chatObjArr){
-    let dayArray = [{Day: "Monday", Count: 0, Percent: 0}, {Day: "Tuesday", Count: 0, Percent: 0}, {Day: "Wednesday", Count: 0, Percent: 0}, {Day: "Thursday", Count: 0, Percent: 0}, {Day: "Friday", Count: 0, Percent: 0}, {Day: "Saturday", Count: 0, Percent: 0}, {Day: "Sunday", Count: 0, Percent: 0}];
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let totalCount = 0;
-
-    chatObjArr.forEach(x => {
-        let dateString = x.Date;
-        let date = parseInt(dateString.split('/')[0]);
-        let month = parseInt(dateString.split('/')[1]);
-        let year = parseInt(dateString.split('/')[2]);
-
-        let dateBuilder = new Date();
-        dateBuilder.setDate(date);
-        dateBuilder.setMonth(month- 1);
-        dateBuilder.setFullYear(year);
+function GenerateMessageDays(chatObjArr) {
+    const dayArray = [
+        { Day: "Monday", Count: 0, Percent: 0 },
+        { Day: "Tuesday", Count: 0, Percent: 0 },
+        { Day: "Wednesday", Count: 0, Percent: 0 },
+        { Day: "Thursday", Count: 0, Percent: 0 },
+        { Day: "Friday", Count: 0, Percent: 0 },
+        { Day: "Saturday", Count: 0, Percent: 0 },
+        { Day: "Sunday", Count: 0, Percent: 0 }
+    ];
+    
+    const totalCount = chatObjArr.length;
+    
+    for (const x of chatObjArr) {
+        const [day, , , year] = x.Date.split('/');
+        const dateBuilder = new Date(year, parseInt(day) - 1);
+        const dayOfWeek = dateBuilder.toLocaleDateString('en-US', { weekday: 'long' });
         
-        let day = days[dateBuilder.getDay()];
-
-        dayArray.forEach(y => day === y.Day ? y.Count ++ : null);
-        totalCount ++;
-    });
-
-    let percentTotal = 0;
-    dayArray.forEach(x => {
-        if(dayArray.indexOf(x) == 6){
-            x.Percent = 100 - percentTotal;
-        }else{
-            x.Percent = Math.round((x.Count / totalCount) * 100);
-            percentTotal += x.Percent;
+        const foundDay = dayArray.find(y => y.Day === dayOfWeek);
+        if (foundDay) {
+            foundDay.Count++;
         }
-    });
-
+    }
+    
+    let percentTotal = 0;
+    for (const day of dayArray) {
+        if (dayArray.indexOf(day) === 6) {
+            day.Percent = 100 - percentTotal;
+        } else {
+            day.Percent = Math.round((day.Count / totalCount) * 100);
+            percentTotal += day.Percent;
+        }
+    }
+    
     return new MessageDays(dayArray);
 }
 
 /**Generates a Message Times metric module */
-function GenerateMessageTimes(chatObjArr){
-    let timeArray= [];
-    for(let i = 0; i <10; i++){
-        timeArray.push({Hour: "0" + i.toString(), Count: 0});
+function GenerateMessageTimes(chatObjArr) {
+    const timeArray = Array.from({ length: 24 }, (_, i) => ({ Hour: i.toString().padStart(2, '0'), Count: 0 }));
+    
+    for (const x of chatObjArr) {
+        const hour = parseInt(x.Time.split(':')[0]);
+        timeArray[hour].Count++;
     }
-    for(let i = 10; i <24; i++){
-        timeArray.push({Hour: i.toString(), Count: 0});
-    }
-
-    chatObjArr.forEach(x => {
-        let hour = x.Time.split(':')[0];
-        timeArray.forEach(y => {
-            if(hour === y.Hour){
-                y.Count ++;
-            }
-        });
-    });
-
+    
     return new MessageTimes(timeArray);
 }
 
 /**Generates a Top Words metric module */
-function GenerateTopWords(wholeChatString, namesArray){
-    let topWordsTable = new Array();
-    let punctuationRegEx = new RegExp(PunctuationRegEx, "g");
-    let numberRegEx = new RegExp(NumberRegEx, "g");
-    let newNameArray = [];
-    namesArray.forEach(x => {
-        x.split(" ").forEach(y => newNameArray.push(y));
-    });
+function GenerateTopWords(wholeChatString, namesArray) {
+    const topWordsTable = [];
+    const punctuationRegEx = new RegExp(PunctuationRegEx, 'g');
+    const numberRegEx = new RegExp(NumberRegEx, 'g');
+    const newNameArray = namesArray.flatMap(x => x.split(' '));
+    
+    // Split entire chat into words Array
+    const wordsArray = wholeChatString.replace(/(’s)/g, '').replace(/('s)/g, '').split(' ');
 
-    //Split entire chat into words Array
-    let wordsArray = wholeChatString.replace(/(’s)/g,"").replace(/('s)/g,"").split(' ');
-
-    //Making sure we don't include punctuation, emojis, numbers, or skipwords in this table
-    let filteredArray = wordsArray.filter(x => !SkipWords.includes(x) && !EmojiArray.includes(x) && !x.match(punctuationRegEx) && !x.match(numberRegEx) && !newNameArray.includes(x) && x.length < 10);
-    let counts = {};
+    // Making sure we don't include punctuation, emojis, numbers, or skipwords in this table
+    const filteredArray = wordsArray.filter(x => !SkipWords.includes(x) && !EmojiArray.includes(x) && !x.match(punctuationRegEx) && !x.match(numberRegEx) && !newNameArray.includes(x) && x.length < 10);
+    const counts = {};
     for (const num of filteredArray) {
         counts[num] = counts[num] ? counts[num] + 1 : 1;
     }
     
     for (const property in counts) {
-	    topWordsTable.push({Word: property, Count: counts[property]});
+        topWordsTable.push({ Word: property, Count: counts[property] });
     }
 
-    return new TopWords(topWordsTable.sort((a, b) => b.Count - a.Count).slice(0,15));
+    return new TopWords(topWordsTable.sort((a, b) => b.Count - a.Count).slice(0, 15));
 }
 
 /**Generates a composite of all messages until the next author in a chat */
-function GetMessageComposite(chatObjArr, replierIndex, message){
-    if(replierIndex > 1){
-        for(let x = 1; x < replierIndex; x++){
-            let currentMessage = chatObjArr[x];
+function GetMessageComposite(chatObjArr, replierIndex, message) {
+    if (replierIndex > 1) {
+        for (let x = 1; x < replierIndex; x++) {
+            const currentMessage = chatObjArr[x];
 
-            let puncRegEx = new RegExp(PunctuationRegEx,"g");
+            const puncRegEx = new RegExp(PunctuationRegEx, 'g');
 
-            if(message.match(puncRegEx)){
-                message += " " + currentMessage["MessageBody"];
-            }else{
-                message += ". " + currentMessage["MessageBody"];
+            if (message.match(puncRegEx)) {
+                message += ` ${currentMessage.MessageBody}`;
+            } else {
+                message += `. ${currentMessage.MessageBody}`;
             }
         }
     }
     return message;
 }
 
-export {GenerateChatComposition, GenerateFirstEncounter, GenerateMessageDays, GenerateMessageTimes, GenerateSearchRecord, GenerateTopWords};
+export {
+    GenerateChatComposition,
+    GenerateFirstEncounter,
+    GenerateMessageDays,
+    GenerateMessageTimes,
+    GenerateSearchRecord,
+    GenerateTopWords
+};
