ff08c35892ec4e160168461e8c5485371f691f43
Michael McGranaghan <michaelmcgranaghan@formuspro.com>
2023-06-22 17:10:46 +0100

    litecontroller refactor

--- controllers/litecontroller.js
+++ controllers/litecontroller.js
@@ -1,9 +1,14 @@
-import { GenerateSearchRecord } from "./metricmodulecontroller.js";
+import { GenerateSearchRecord } from './metricmodulecontroller.js';
 
-/**Count instances of a personal word*/
-async function CountPersonalWord(chatObjArr, personalWord){
-    let searchTermArr = [personalWord];
-    return GenerateSearchRecord(chatObjArr,null,true,null,null,searchTermArr);
+/**
+ * Count instances of a personal word
+ * @param {Array} chatObjArr - Array of chat objects
+ * @param {string} personalWord - Personal word to count
+ * @returns {Promise} - Promise that resolves to the search record
+ */
+async function CountPersonalWord(chatObjArr, personalWord) {
+  const searchTermArr = [personalWord];
+  return GenerateSearchRecord(chatObjArr, null, true, null, null, searchTermArr);
 }
 
-export {CountPersonalWord}
+export { CountPersonalWord };
