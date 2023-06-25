17a3526124070cdcbc210257a40e92d8d08ccd0d
Michael McGranaghan <michaelmcgranaghan@formuspro.com>
2023-06-25 13:49:25 +0100

    product.js refactor 2

--- models/products.js
+++ models/products.js
@@ -1,45 +1,45 @@
 /**Represents the Product class which all CC products will be a sub-class of.*/
 class Product {
-    constructor(productName, productCode, sendRequestToPoD){
+    constructor(productName, productCode, sendRequestToPoD) {
         this.ProductName = productName;
         this.ProductCode = productCode;
         this.SendRequestToPoD = sendRequestToPoD;
     }
 }
 
-/**Represents a container for a Chat Chart*/
 class ChatChart extends Product {
-    constructor(productCode, chatters, startDate, endDate){
+    constructor(productCode, chatters, startDate, endDate) {
         super("Chat Chart", productCode, false);
         this.Chatters = chatters;
         this.StartDate = startDate;
         this.EndDate = endDate;
-        this.MetricModules = new Array();
+        this.MetricModules = new Map();
         this.ProgressPercent = 0;
     }
 
     AddMetricModule(metricModule) {
-        let areaSum = this.MetricModules.reduce((sum, x) => sum + x.Area, 0);
-        
+        let areaSum = 0;
+        for (const value of this.MetricModules.values()) {
+            areaSum += value.Area;
+        }
+
         if (areaSum > 24) {
             alert("Sorry, we can't fit any more graphics on your Chat Chart! Try removing something.");
         } else {
-            this.MetricModules.push(metricModule);
-            let modulePercent = (metricModule.Area / 24) * 100;
-            this.ProgressPercent += Math.round(modulePercent);
+            this.MetricModules.set(metricModule, true);
+            this.ProgressPercent += Math.round((metricModule.Area * 100) / 24);
         }
     }
 
     RemoveMetricModule(metricModule) {
-        const index = this.MetricModules.findIndex(x => x === metricModule);
-        if (index === -1) { return; }
-    
-        let modulePercent = (metricModule.Area / 24) * 100;
-        this.ProgressPercent -= Math.round(modulePercent);
-        this.MetricModules.splice(index, 1);
-    }    
+        if (!this.MetricModules.has(metricModule)) { return; }
+
+        const moduleArea = metricModule.Area;
+        this.ProgressPercent -= Math.round((moduleArea * 100) / 24);
+        this.MetricModules.delete(metricModule);
+    }
 }
 
 export {
     ChatChart
-}
+};
