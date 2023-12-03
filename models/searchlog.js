3c6c247e07b71eb3660b18a03945bd4bd45e4e51
mikemickymick <54855355+mikemickymick@users.noreply.github.com>
2023-12-03 15:39:04 +0000

    Delete models directory

--- models/searchlog.js
+++
@@ -1,9 +0,0 @@
-/**Represents a key-value pairing of a search term and it's count.*/
-class SearchLog {
-    constructor(searchTerm, count){
-        this.SearchTerm = searchTerm;
-        this.Count = count;
-    }
-}
-
-export {SearchLog}
